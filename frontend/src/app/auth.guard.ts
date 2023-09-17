db80b07b5f9e837e13273bba157d30ae4575daaf
forMedusa <amaan5054@gmail.com>
2023-09-17 23:08:24 +0530

    Auth Guard error fixed

--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -1,8 +1,10 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core' 
+import { CookieService } from 'ngx-cookie-service';
 export const authGuard: CanActivateFn = (route, state) => {
     let router = inject(Router);
-    let authenticatedUser = localStorage.getItem('id');
+    let cookies = inject(CookieService);
+    let authenticatedUser = cookies.get('id');
     if(authenticatedUser){
       return true;
     }else{
